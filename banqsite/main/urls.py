4f9f627972b9f56fa3f6efaddcba58a5391635ef
nathan-77 <47172100+n-athan-77@users.noreply.github.com>
2019-10-19 18:35:14 +0530

    djangoic login logout

--- banqsite/main/urls.py
+++ banqsite/main/urls.py
@@ -7,8 +7,9 @@ app_name = 'main'  # here for namespacing of urls.
 urlpatterns = [
     path("", views.home, name="home"),
     path('chatbot/', views.chatbot, name='chatbot'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_req, name='login'),
     path('account/', views.account, name='account'),
+    path('logout/', views.logout_request, name='logout'),
     
 
 ]
